f0bdfe47f975891c62223b09fee047a09776e740
xsyo <xsyu1324@mail.ru>
2021-01-08 21:09:12 +0600

    gunicorn

--- config/settings.py
+++ config/settings.py
@@ -78,11 +78,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql_psycopg2',
+'NAME': 'cloudproject',
+'USER': 'clouduser',
+'PASSWORD': 'password',
+'HOST': 'localhost',
+'PORT': '',
+}
 }
 
 
